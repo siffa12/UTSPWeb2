1dd61cd4abe16d8d52691b441046002208cd27af
siffa12 <72341633+siffa12@users.noreply.github.com>
2022-05-20 01:50:34 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,40 @@
+<!-- Fixed navbar -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <a class="navbar-brand" href="?page=utama">Sistem Surat Keluar Dan Masuk</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <!--li class="active"><a href="#">Home</a></li-->
+
+                <?php if(isset($_SESSION['level']) && $_SESSION['level']==1) { ?>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu Surat <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="?page=s_masuk&actions=tampil">Surat Masuk</a></li>
+                        <li><a href="?page=s_keluar&actions=tampil">Surat Keluar</a></li>
+                    </ul>
+                </li>
+
+
+                <?php } ?>
+
+
+
+                    <?php if (isset($_SESSION['username'])) { ?>
+                    <li><a href="logout.php">Logout</a></li>
+                <?php } ?>
+
+            </ul>
+        </div>
+    </div>
+</nav>
